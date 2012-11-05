d380d87eefd187232c62cd5824fd3a5f84b95018
Myron Yao <solfalling.lunarising@hotmail.com>
2012-11-04 21:38:17 -0800

    Update project1.hs

--- project1.hs
+++ project1.hs
@@ -3,43 +3,46 @@
 -- UBC CPSC 312 Functional Project
 -- Nov 4th 2012
 
-type OskaBoard = [String]
 
-data Turn = wht | blk			-- white | black
-data Movetype = adv | jmp		-- advance | jump
-data Direction = l | r			-- left | right
+import Data.Maybe
+
+type OskaBoard = [String]
+type Turn = Char						-- white | black
 type Coordinate = (Int, Int, Turn)
 type Move = (Coordinate, Movetype, Direction)
+data Movetype = Adv | Jmp	deriving (Eq)	-- advance | jump
+data Direction = L | R		deriving (Eq)	-- left | right
 
 
 oska_v4o6 :: OskaBoard -> Turn -> Int -> OskaBoard
-oska_v4o6 board turn depth	= first (oska_helper board turn depth)
+oska_v4o6 board turn depth	= fst (oska_helper board turn depth)
 
 
 -- is recursive
 oska_helper :: OskaBoard -> Turn -> Int -> (OskaBoard, Int)
 oska_helper board turn depth
-	| depth == 0	= board
-	| depth == 1	= minmax nextMovesGoodVals (opponent turn)
-	| otherwise	= minmax [ oska_helper board0 | board0 <- nextMoves ] (opponent turn)
-	where		nextMoves = move_gen board turn
-			nextMovesGoodVals = evaluate_list nextMoves
+	| depth == 0	= evaluate_board (board, turn)
+	| depth == 1	= minmax nextMoveVals (opponent turn)
+	| otherwise	= minmax [ oska_helper board0 (opponent turn) (depth - 1) | board0 <- nextMoves ] (opponent turn)
+	where
+		nextMoves = move_gen board turn
+		nextMoveVals = evaluate_list nextMoves turn
 
 
-evaluate_list :: [OskaBoard] -> [(OskaBoard, Int)]
-evaluate_list boardlist	= map static_board_evaluator boardlist
+evaluate_list :: [OskaBoard] -> Turn -> [(OskaBoard, Int)]
+evaluate_list boardlist	turn = map evaluate_board (zip boardlist (repeat turn))
 
 -- TODO
 -- our heuristic function
-static_board_evaluator :: OskaBoard -> Turn -> (OskaBoard, Int)
-evaluateOneBoard board turn		= (board, val)
+evaluate_board :: (OskaBoard, Turn) -> (OskaBoard, Int)
+evaluate_board (board, t)		= (board, val)
 	where
-		pawns = find_pawns board turn
+		pawns = find_pawns board t
 		b_length = length board
 		val = sum [ b_length - r | (r, c, t) <- pawns ]
 
 
---	| turn == 'W'	= (board, (((length board) * (length (head board))) - (0.8 * fromIntegral(sum_of_dist - sum_of_dist_oppo))))
+--	| turn == 'w'	= (board, (((length board) * (length (head board))) - (0.8 * fromIntegral(sum_of_dist - sum_of_dist_oppo))))
 --	| otherwise	= (board, (negate ((((length board) * (length (head board))) - (0.8 * fromIntegral(sum_of_dist - sum_of_dist_oppo))))))
 --	where dest_row = find_dest_row board turn
 --	      dest_row_oppo = find_dest_row board (opponent turn)
@@ -50,45 +53,54 @@ evaluateOneBoard board turn		= (board, val)
 	      --locally defined function
 --	      find_dest_row :: OskaBoard -> Char -> Int
 --	      find_dest_row board turn
---		| turn == 'W'	= (length board) - 1
+--		| turn == 'w'	= (length board) - 1
 --		| otherwise	= 0
+
 
 
 -- returns the other turn
 opponent :: Turn -> Turn
 opponent turn
-	| turn == wht		= blk
-	| otherwise		= wht
+	| turn == 'w'	= 'b'
+	| otherwise		= 'w'
 
 
 -- returns tuple with highest value if white, lowest value if black
 minmax :: [(OskaBoard, Int)] -> Turn -> (OskaBoard, Int)
 minmax moves turn
-	| turn == wht		= max (map second moves)
-	| otherwise		= min (map second moves)
+	| turn == 'w'		= heur_max moves
+	| otherwise		= heur_min moves
 
-max :: [(OskaBoard, Int)] -> (OskaBoard, Int)
-max []	= []
-max [b]	= [b]
-max [(b,val):bs)
-	| val > maxrest		= (b,val)
-	| otherwise		= maxrest
-	where		maxrest = max bs
+heur_max:: [(OskaBoard, Int)] -> (OskaBoard, Int)
+heur_max [b]			= b
+heur_max (b:bs)
+	| val > (snd maxR)	= b
+	| otherwise			= maxR
+	where
+		val = snd b
+		maxR = heur_max bs
 
-min :: [(OskaBoard, Int)] -> (OskaBoard, Int)
-min []	= []
-min [b]	= [b]
-min [(b,val):bs)
-	| val < minrest		= (b,val)
-	| otherwise		= minrest
-	where		minrest = min bs
+heur_min:: [(OskaBoard, Int)] -> (OskaBoard, Int)
+heur_min [b]			= b
+heur_min (b:bs)
+	| val > (snd minR)	= b
+	| otherwise			= minR
+	where
+		val = snd b
+		minR = heur_min bs
 
 
 -- move generator for new Oska states
 move_gen :: OskaBoard -> Turn -> [OskaBoard]
-move_gen board turn		= [ doMove board m | m <- moves ]
-	where		moves = concat (map (find_legal_moves pawns))
-			pawns = find_pawns board turn
+move_gen board turn	= [ doMove board m | m <- moves ]
+	where
+		pawns = find_pawns board turn
+		moves = concat (map find_legal_moves pawns)
+
+		find_legal_moves (r, c, turn)
+			| turn == 'w'	= white_check_row r c b_length (take 3 (drop r board))
+			| otherwise		= black_check_row r c b_length (take 3 (drop r (reverse board)))
+		b_length = length board
 
 
 -- Finding the coordinate of all pawns
@@ -97,15 +109,16 @@ move_gen board turn		= [ doMove board m | m <- moves ]
 -- @output all the coordinate associate with the pawn in the current board
 find_pawns :: OskaBoard -> Turn -> [Coordinate]
 find_pawns board turn
-	| turn == wht		= scan_rows board wht 0
-	| otherwise		= scan_rows (reverse board) blk 0
+	| turn == 'w'		= scan_rows board 'w' 0
+	| otherwise		= scan_rows (reverse board) 'b' 0
 
 -- recursive helper function: scans a list of rows
 scan_rows :: OskaBoard -> Turn -> Int -> [Coordinate]
 scan_rows rows turn i
 	| null rows		= []
 	| otherwise		= (scan_row row_i turn i 0) ++ (scan_rows (tail rows) turn (i + 1))
-	where	row_i	= head rows
+	where
+		row_i	= head rows
 
 -- recursive helper function: scans a row
 scan_row :: String -> Turn -> Int -> Int -> [Coordinate]
@@ -115,38 +128,35 @@ scan_row row turn i j
 	| otherwise		= scan_row (tail row) turn i (j + 1)
 
 
-find_legal_moves board (r, c, turn)
-	| turn == w	= white_check_row r c b_length (take 3 (drop r board))
-	| otherwise	= black_check_row r c b_length (take 3 (drop r (reverse board)))
-	where 	b_length = length board
-
+-- main work function for find_legal_moves (turn = 'w')
 white_check_row :: Int -> Int -> Int -> [String] -> [Move]
 white_check_row r c b_length rows
 	| null row1		= []
 	| otherwise		= w_check
-	where	wNoJ = null row2
+	where
+		wNoJ = null row2
 		wR = (c == 0)
 		wL = ((c - 1) == length row0)
 
-		w_check		| wNoJ		= wNoJ_check
-				| wR		= wR_check : []
-				| wL		= wL_check : []
-				| otherwise	= wR_check : wL_check : []
+		w_check	| wNoJ	= wNoJ_check
+				| wR		= wR_check
+				| wL		= wL_check
+				| otherwise	= wR_check ++ wL_check
+		wNoJ_check	| wR		= wRA_check
+				| wL		= wLA_check
+				| otherwise	= wRA_check ++ wLA_check
 
-		wNoJ_check	| wR		= wRA_check : []
-				| wL		= wLA_check : []
-				| otherwise	= wRA_check : wLA_check : []
-		wR_check	| wRA		= ((r,c,W), A, R)
-				| wRJ		= ((r,c,W), J, R)
-				| otherwise	= Nothing
-		wL_check	| wLA		= ((r,c,W), A, L)
-				| wLJ		= ((r,c,W), J, L)
-				| otherwise	= Nothing
+		wR_check	| wRA		= ((r,c,'w'), Adv, R) : []
+				| wRJ		= ((r,c,'w'), Jmp, R) : []
+				| otherwise	= []
+		wL_check	| wLA		= ((r,c,'w'), Adv, L) : []
+				| wLJ		= ((r,c,'w'), Jmp, L) : []
+				| otherwise	= []
 
-		wRA_check	| wRA		= ((r,c,W), A, R)
-				| otherwise	= Nothing
-		wLA_check	| wLA		= ((r,c,W), A, L)
-				| otherwise	= Nothing
+		wRA_check	| wRA		= ((r,c,'w'), Adv, R) : []
+				| otherwise	= []
+		wLA_check	| wLA		= ((r,c,'w'), Adv, L) : []
+				| otherwise	= []
 
 		wRA = (wR1 == '_')
 		wLA = (wL1 == '_')
@@ -172,45 +182,46 @@ white_check_row r c b_length rows
 		row2 = last rows
 
 
+-- main work function for find_legal_moves (turn = 'b')
 black_check_row :: Int -> Int -> Int -> [String] -> [Move]
 black_check_row r c b_length rows
 	| null row1		= []
 	| otherwise		= b_check
-	where	bNoJ = null row2
+	where
+		bNoJ = null row2
 		bR = (c == 0)
 		bL = ((c - 1) == length row0)
 
 		b_check
-			| bNoJ		= bNoJ_check
-			| bR		= bR_check : []
-			| bL		= bL_check : []
-			| otherwise	= bR_check : bL_check : []
-
+			| bNoJ	= bNoJ_check
+			| bR		= bR_check
+			| bL		= bL_check
+			| otherwise	= bR_check ++ bL_check
 		bNoJ_check
-			| bR		= bRA_check : []
-			| bL		= bLA_check : []
-			| otherwise	= bRA_check : bLA_check : []
+			| bR		= bRA_check
+			| bL		= bLA_check
+			| otherwise	= bRA_check ++ bLA_check
 
 		bR_check
-			| bRA		= ((r,c,b), A, R)
-			| bRJ		= ((r,c,b), J, R)
-			| otherwise	= Nothing
+			| bRA		= ((r,c,'w'), Adv, R) : []
+			| bRJ		= ((r,c,'w'), Jmp, R) : []
+			| otherwise	= []
 		bL_check
-			| bLA		= ((r,c,b), A, L)
-			| bLJ		= ((r,c,b), J, L)
-			| otherwise	= Nothing
+			| bLA		= ((r,c,'w'), Adv, L) : []
+			| bLJ		= ((r,c,'w'), Jmp, L) : []
+			| otherwise	= []
 
 		bRA_check
-			| bRA		= ((r,c,b), A, R)
-			| otherwise	= Nothing
+			| bRA		= ((r,c,'w'), Adv, R) : []
+			| otherwise	= []
 		bLA_check
-			| bLA		= ((r,c,b), A, L)
-			| otherwise	= Nothing
+			| bLA		= ((r,c,'w'), Adv, L) : []
+			| otherwise	= []
 
 		bRA = (bR1 == '_')
 		bLA = (bL1 == '_')
-		bRJ = (bR1 == 'b') && (bR2 == '_')
-		bLJ = (bR1 == 'b') && (bL2 == '_')
+		bRJ = (bR1 == 'w') && (bR2 == '_')
+		bLJ = (bR1 == 'w') && (bL2 == '_')
 
 		bR1	| r10diff < 0 		= head (drop r row1)
 			| otherwise		= head (drop (r + 1) row1)
@@ -231,7 +242,7 @@ black_check_row r c b_length rows
 		row2 = last rows
 
 
--- advances a pawn according to the paramters and return the board after that 
+-- advances a pawn according to the paramters and return the board after that
 -- move.
 -- @param board OskaBoard the current state of the broad
 -- @param coord Coordinate the pawn that is being moved
@@ -239,28 +250,29 @@ black_check_row r c b_length rows
 -- @output the oska board after the move advances a pawn
 advance_pawn :: OskaBoard -> Coordinate -> Direction -> OskaBoard
 advance_pawn board (r, c, t) d
-	| t == wht		= advance_pawn_helper board t r c d
+	| t == 'w'		= advance_pawn_helper board t r c d
 	| otherwise		= reverse (advance_pawn_helper (reverse board) t r c d)
 
 -- helper function
 advance_pawn_helper :: OskaBoard -> Turn -> Int -> Int -> Direction -> OskaBoard
 advance_pawn_helper rows t r c d	= prefix_rows ++ new_row0 : new_row1 : suffix_rows
-	where		(prefix_rows, remainder) = splitAt r rows
-			row0 = head remainder
-			row1 = head (tail remainder)
-			suffix_rows = drop 2 remainder
+	where
+		(prefix_rows, remainder) = splitAt r rows
+		row0 = head remainder
+		row1 = head (tail remainder)
+		suffix_rows = drop 2 remainder
 
-			r10diff = (length row1) - (length row0)
-			d_shift	| d == R	= 0
-				| d == L	= -1
+		r10diff = (length row1) - (length row0)
+		d_shift	| d == R	= 0
+				| otherwise	= -1
 
-			(r0_prefix, r0_suffix) = splitAt c row0
-			new_row0 = r0_prefix ++ '_' : (tail r0_suffix)
-			
-			(r1_prefix, r1_suffix)
-				| r10diff < 0	= splitAt (c + d_shift) row1
-				| otherwise		= splitAt (c + d_shift + 1) row1
-			new_row1 = r1_prefix ++ t : (tail r1_suffix)
+		(r0_prefix, r0_suffix) = splitAt c row0
+		new_row0 = r0_prefix ++ '_' : (tail r0_suffix)
+
+		(r1_prefix, r1_suffix)
+			| r10diff < 0	= splitAt (c + d_shift) row1
+			| otherwise		= splitAt (c + d_shift + 1) row1
+		new_row1 = r1_prefix ++ t : (tail r1_suffix)
 
 
 -- jumps a pawn based on the parameter provided.
@@ -268,15 +280,15 @@ advance_pawn_helper rows t r c d	= prefix_rows ++ new_row0 : new_row1 : suffix_r
 -- @param coord Coordinate the pawn that is being moved
 -- @param drtn Direction whether the pawn should move left/right along the diagonal
 -- @output the oska board after the move
-jump_pawn :: OskaBoard -> Coordinate -> Direction -> Oskaboard
+jump_pawn :: OskaBoard -> Coordinate -> Direction -> OskaBoard
 jump_pawn board (r, c, t) d
-	| t == wht		= jump_pawn_helper board t r c d
+	| t == 'w'		= jump_pawn_helper board t r c d
 	| otherwise		= reverse (jump_pawn_helper (reverse board) t r c d)
 
 -- helper function
 jump_pawn_helper :: OskaBoard -> Turn -> Int -> Int -> Direction -> OskaBoard
 jump_pawn_helper rows t r c d	= prefix_rows ++ new_row0 : new_row1 : new_row2 : suffix_rows
-	where	
+	where
 		(prefix_rows, remainder) = splitAt r rows
 		row0 = head remainder
 		row1 = head (tail remainder)
@@ -290,7 +302,7 @@ jump_pawn_helper rows t r c d	= prefix_rows ++ new_row0 : new_row1 : new_row2 : 
 
 		(r0_prefix, r0_suffix) = splitAt c row0
 		new_row0 = r0_prefix ++ '_' : (tail r0_suffix)
-			
+
 		(r1_prefix, r1_suffix)
 			| r10diff < 0	= splitAt (c + d_shift) row1
 			| otherwise		= splitAt (c + d_shift + 1) row1
@@ -303,8 +315,8 @@ jump_pawn_helper rows t r c d	= prefix_rows ++ new_row0 : new_row1 : new_row2 : 
 		new_row2 = r2_prefix ++ t : (tail r2_suffix)
 
 
-doMove :: Oskaboard -> Move -> OskaBoard
+doMove :: OskaBoard -> Move -> OskaBoard
 doMove board (c, m, d)
-	| m == advance		= advance_pawn board c d
+	| m == Adv		= advance_pawn board c d
 	| otherwise		= jump_pawn board c d
 
